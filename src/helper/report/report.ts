72ee4b9da2ca78c5ac4718867a3a4565dcaa9d18
ModtihAFG <modith.george@afgonline.com.au>
2023-12-07 11:48:48 +0800

    updated package json files8

--- src/helper/report/report.ts
+++ src/helper/report/report.ts
@@ -2,7 +2,7 @@ const report = require("multiple-cucumber-html-reporter");
 
 report.generate({
     jsonDir: "test-results",
-    reportPath: "test-results/",
+    reportPath: "test-results/report",
     reportName: "Playwright Automation Report",
     pageTitle: "BookCart App test report",
     displayDuration: false,
